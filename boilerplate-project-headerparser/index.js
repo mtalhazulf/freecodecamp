c3f93f969f19a7ae3b5880f75c6725645e455aeb
Muhammad Talha <talhazulf4163@gmail.com>
2024-03-30 11:37:45 +0500

    all_uP

---
+++ boilerplate-project-headerparser/index.js
@@ -0,0 +1,51 @@
+// index.js
+// where your node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', function (req, res) {
+
+  //Parset and Extract this info 
+
+  /*
+    {"ipaddress":"::ffff:159.20.14.100","language":"en-US,en;q=0.5",
+"software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}
+  */
+  const header = req.headers
+  console.log(header);
+  var ipaddress = header['x-forwarded-for'] || req.connection.remoteAddress;
+  var language = header['accept-language'];
+  var software = header['user-agent'];
+
+
+  return res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software
+  });
+});
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
